a2f8ef62d7a2cedc56db2b8c7a29477bde05d09a
lucky-ponta <soraali1225@gmail.com>
2020-06-11 13:18:50 +0900

    FINISH 4-1”
    a
    ;

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'bdc77412a95cd56bc45135a2c7aec345ea919cc13fdfa0d7c2d9f753a5c46b0e027dfc4705ad72602ac13b56c76106c71fd3d7ea0bf733b03bc163ce13858a36'
